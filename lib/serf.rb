1426c47b7f098e28ef5f5aebff845d575d18f374
Benjamin Yu <benjaminlyu@gmail.com>
2011-12-11 13:20:39 -0800

    Removes all the autoloads from serf.rb
    Details:
    * autoload is getting deprecated.
    * Going to reorg this library.

--- lib/serf.rb
+++ lib/serf.rb
@@ -1,21 +1,4 @@
 module Serf
-
-  autoload :Builder, 'serf/builder'
-  autoload :NullObject, 'serf/null_object'
-
-  # Emitters
-  autoload :RedisEmitter, 'serf/emitters/redis_emitter'
-
-  # Receivers
-  autoload :MsgpackReceiver, 'serf/receivers/msgpack_receiver'
-  autoload :RedisPubsubReceiver, 'serf/receivers/redis_pubsub_receiver'
-
-  module Middleware
-    autoload :CelluloidRunner, 'serf/middleware/celluloid_runner'
-    autoload :EmRunner, 'serf/middleware/em_runner'
-    autoload :KindMapper, 'serf/middleware/kind_mapper'
-  end
-
 end
 
 require 'serf/version'
