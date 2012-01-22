9d5eadee07cd885657bee05bdc19d883fbc6eb44
Benjamin Yu <benjaminlyu@gmail.com>
2012-01-22 12:01:34 -0800

    Refactor with_error_handling wrapper into util module.
    Details:
    * Exception handling functionality can be reused by other classes.

--- lib/serf/runners/direct_runner.rb
+++ lib/serf/runners/direct_runner.rb
@@ -1,5 +1,4 @@
-require 'serf/messages/caught_exception_event'
-require 'serf/util/null_object'
+require 'serf/util/with_error_handling'
 
 module Serf
 module Runners
@@ -10,20 +9,16 @@ module Runners
   # to proper error or results channels.
   #
   class DirectRunner
+    include ::Serf::Util::WithErrorHandling
 
     def initialize(options={})
       # Mandatory, we want both results and error channels.
       @results_channel = options.fetch(:results_channel)
       @error_channel = options.fetch(:error_channel)
 
-      # For caught exceptions, we're going to publish an error event
-      # over our error channel. This defines our error event message class.
-      @error_event_class = options.fetch(:error_event_class) {
-        ::Serf::Messages::CaughtExceptionEvent
-      }
-
-      # Our default logger
-      @logger = options.fetch(:logger) { ::Serf::Util::NullObject.new }
+      # Optional overrides for error handling
+      @error_event_class = options[:error_event_class]
+      @logger = options[:logger]
     end
 
     def run(handler, params)
@@ -48,25 +43,6 @@ module Runners
         end
       end
       return nil
-    end
-
-    ##
-    # A block wrapper to handle errors when executing a block.
-    #
-    def with_error_handling(context=nil)
-      yield
-    rescue => e
-      error_event = @error_event_class.new(
-        context: context,
-        error_message: e.inspect,
-        error_backtrace: e.backtrace.join("\n"))
-
-      # log the error to our logger, and to our error channel.
-      @logger.error error_event
-      @error_channel.publish error_event
-
-      # We're done, so just return this error.
-      return error_event
     end
   end
 
---
+++ lib/serf/util/with_error_handling.rb
@@ -0,0 +1,44 @@
+require 'serf/messages/caught_exception_event'
+require 'serf/util/null_object'
+
+module Serf
+module Util
+
+  ##
+  # Helper module to rescues exceptions from executing blocks of
+  # code, and then logs+publishes the error event.
+  #
+  module WithErrorHandling
+
+    ##
+    # A block wrapper to handle errors when executing a block.
+    #
+    # Including classes may have the following instance variables
+    # to override the default values:
+    # * @error_event_class - ::Serf::Messages::CaughtExceptionEvent
+    # * @logger - ::Serf::Util::NullObject.new
+    # * @error_channel - ::Serf::Util::NullObject.new
+    #
+    def with_error_handling(context=nil)
+      yield
+    rescue => e
+      eec = @error_event_class || ::Serf::Messages::CaughtExceptionEvent
+      logger = @logger || ::Serf::Util::NullObject.new
+      error_channel = @error_channel || ::Serf::Util::NullObject.new
+      error_event = eec.new(
+        context: context,
+        error_message: e.inspect,
+        error_backtrace: e.backtrace.join("\n"))
+
+      # log the error to our logger, and to our error channel.
+      logger.error error_event
+      error_channel.publish error_event
+
+      # We're done, so just return this error.
+      return error_event
+    end
+
+  end
+
+end
+end
