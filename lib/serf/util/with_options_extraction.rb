2c6571366bbf252ce05e3913b332ff14988960c1
Benjamin Yu <benjaminlyu@gmail.com>
2012-02-27 09:05:10 -0800

    Changes Semantics of opts and opts! helpers.
    Details:
    * Adds opts! method, which will raise an error when a nil would have
      been returned as the found options.
    * Changes opts to not raise an error at all.
    * Changes opts to return the default value if the key is
      non-existent in the options value, or if the explicit value
      of the key in the options hash is set to nil.

--- lib/serf/util/with_options_extraction.rb
+++ lib/serf/util/with_options_extraction.rb
@@ -35,14 +35,59 @@ module Util
     protected
 
     ##
-    # Helper method to get an option from the @options hash.
+    # Helper method to lookup an option from our options hash.
+    #
+    # Examples:
+    #
+    #   # Optional parameter whose default value is nil.
+    #   do_extra = opts :do_extra
+    #
+    #   # Optional params that defaults to [1,2,3]
+    #   start_array = opts :start_array, [1,2,3]
+    #
+    # Returns default value when:
+    # * Key is non-existent in Options Hash.
+    # * OR when the value of the key in the Options Hash is nil.
+    #
+    # Returns nil when:
+    # * Default is nil and the Options Hash lookup returns a nil.
+    #   Options Hash returns a nil because of either a
+    #   non-existent key or a nil value in said hash for said key.
     #
     def opts(key, default=nil)
-      if default.nil?
-        return options.fetch key
-      else
-        return options.fetch(key) { default }
-      end
+      value = options[key]
+      value = default if value.nil?
+      return value
+    end
+
+    ##
+    # Use this lookup helper if a nil value is unacceptable for processing.
+    #
+    # There are cases where an option may have a default value for a feature,
+    # but the caller may just want to disable said feature. To do so,
+    # users of this module should allow for the caller to pass in 'false'
+    # as an option value instead of nil to disable said feature. The
+    # implementer will have to do a boolean check of the returned option value
+    # and disable accordingly.
+    #
+    # Examples:
+    #
+    #   # Has a default logger, but we can disable logging by passing false.
+    #   # If caller had passed in nil for :logger, it would have
+    #   # defaulted to ::Log4r['my_logger'].
+    #   logger = opts! :logger, ::Log4r['my_logger']
+    #   logger = Serf::NullObject.new unless logger
+    #
+    #   # Here we force the end user to pass in a Non-nil option as a
+    #   # mandatory parameter.
+    #   max_threads = opts! :max_threads
+    #
+    # Raises error when `opts` returns a nil.
+    #
+    def opts!(key, default=nil)
+      value = opts key, default
+      raise "Nil value found for option: #{key}, #{default}" if value.nil?
+      return value
     end
 
     ##
