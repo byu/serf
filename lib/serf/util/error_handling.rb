f5107457086a0a1afcfb97ef7efb16f83555e179
Benjamin Yu <benjaminlyu@gmail.com>
2012-03-17 23:16:32 -0700

    Enhances ErrorHandling with ProtectedCall and custom errback.
    Details:
    * ErrorHandling now uses the protected_call module to catch the
      raised exceptions when calling the block.
    * ErrorHandling's code to rescue from the error has been pulled out
      into its own method. This method may be overridden by including classes.
    * with_error_handlings now accepts an additional parameter to
      explicitly declare a different error handling method to call
      in response to an error. This parameter, may instead, be a Proc
      to be called directly.

--- lib/serf/util/error_handling.rb
+++ lib/serf/util/error_handling.rb
@@ -3,6 +3,7 @@ require 'active_support/core_ext/string/inflections'
 require 'serf/messages/caught_exception_event'
 require 'serf/util/null_object'
 require 'serf/util/options_extraction'
+require 'serf/util/protected_call'
 
 module Serf
 module Util
@@ -11,21 +12,34 @@ module Util
   # Helper module to rescues exceptions from executing blocks of
   # code, and then logs+pushes the error event.
   #
+  # Including classes may have the following instance variables
+  # to override the default values:
+  # * @error_event_class - ::Serf::Messages::CaughtExceptionEvent
+  # * @logger - ::Serf::Util::NullObject.new
+  # * @error_channel - ::Serf::Util::NullObject.new
   module ErrorHandling
-    include ::Serf::Util::OptionsExtraction
+    include Serf::Util::OptionsExtraction
+    include Serf::Util::ProtectedCall
 
     ##
     # A block wrapper to handle errors when executing a block.
     #
-    # Including classes may have the following instance variables
-    # to override the default values:
-    # * @error_event_class - ::Serf::Messages::CaughtExceptionEvent
-    # * @logger - ::Serf::Util::NullObject.new
-    # * @error_channel - ::Serf::Util::NullObject.new
+    def with_error_handling(context=nil, errback=:handle_error, &block)
+      ok, results = protected_call &block
+      return ok, (ok ?
+        results :
+        ((errback.is_a?(Proc) ?
+          errback.call(results, context) :
+          send(errback, results, context))))
+    end
+
+    ##
+    # Including classes may override this method to do alternate error
+    # handling. By default, this method will create a new caught exception
+    # event and publish it to the error channel. This method will also
+    # log the exception itself to the logger.
     #
-    def with_error_handling(context=nil)
-      return true, yield
-    rescue => e
+    def handle_error(e, context=nil)
       eec = opts :error_event_class, ::Serf::Messages::CaughtExceptionEvent
       logger = opts :logger, ::Serf::Util::NullObject.new
       error_channel = opts :error_channel, ::Serf::Util::NullObject.new
@@ -35,20 +49,18 @@ module Util
         message: e.message,
         backtrace: e.backtrace.join("\n"))
 
-      # log the error to our logger, and to our error channel.
-      logger.error error_event
+      # log the error to our logger
+      logger.error e
+
+      # log the error event to our error channel.
       begin
         error_channel.push error_event
       rescue => e1
-        logger.error("
-          Failed pushing to ErrorChannel:
-          #{e1.message}
-          #{e1.backtrace.join('\n')}
-        ")
+        logger.error e1
       end
 
       # We're done, so just return this error.
-      return false, error_event
+      return error_event
     end
 
   end
