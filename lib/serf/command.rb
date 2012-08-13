635b06103025aa0fc23f6763f57ae270b9fa3ca2
Benjamin Yu <benjaminlyu@gmail.com>
2012-08-12 23:08:16 -0700

    Rename context to headers; Remove ErrorHandling from Command.
    Details:
    * Slight semantic change that the context is really the
      headers to the message. The combination of which is a "Parcel".
    * Remove the ErrorHandling module from the Command module.

--- lib/serf/command.rb
+++ lib/serf/command.rb
@@ -1,7 +1,6 @@
 require 'active_support/concern'
 require 'active_support/core_ext/class/attribute'
 
-require 'serf/util/error_handling'
 require 'serf/util/options_extraction'
 require 'serf/util/protected_call'
 require 'serf/util/uuidable'
@@ -19,12 +18,12 @@ module Serf
   #       @model = opts :model, MyModel
   #     end
   #
-  #     def call(request, context)
-  #       # Do something w/ request, opts and context.
-  #       item = @model.find request.model_id
+  #     def call(message, headers)
+  #       # Do something w/ message, opts and headers.
+  #       item = @model.find message.model_id
   #       # create a new hashie of UUIDs, which we will use as the base
   #       # hash of our response
-  #       response = create_uuids request
+  #       response = create_uuids message
   #       response.kind = 'my_command/events/did_something'
   #       response.item = item
   #       return response
@@ -33,9 +32,9 @@ module Serf
   #
   #   constructor_params = [1, 2, 3, 4, etc]
   #   block = Proc.new {}
-  #   request = ::Hashie::Mash.new
-  #   context = ::Hashie::Mash.new user: current_user
-  #   MyCommand.call(request, context, *contructor_params, &block)
+  #   message = ::Hashie::Mash.new
+  #   headers = ::Hashie::Mash.new user: current_user
+  #   MyCommand.call(message, headers, *contructor_params, &block)
   #
   module Command
     extend ActiveSupport::Concern
@@ -44,9 +43,8 @@ module Serf
     include Serf::Util::Uuidable
     include Serf::Util::OptionsExtraction
     include Serf::Util::ProtectedCall
-    include Serf::Util::ErrorHandling
 
-    def call(request, context=nil *args, &block)
+    def call(message, headers=nil *args, &block)
       raise NotImplementedError
     end
 
@@ -55,14 +53,14 @@ module Serf
       ##
       # Class method that both builds then executes the unit of work.
       #
-      # @param request the request
-      # @param context the context about the request. Things like the
+      # @param message the message
+      # @param headers the headers about the message. Things like the
       #   requesting :user for ACL.
       # @param *args remaining contructor arguments
       # @param &block the block to pass to constructor
       #
-      def call(request, context=::Hashie::Mash.new, *args, &block)
-        self.build(*args, &block).call(request, context)
+      def call(message, headers=::Hashie::Mash.new, *args, &block)
+        self.build(*args, &block).call(message, headers)
       end
 
       ##
