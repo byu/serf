4d636d3bfcabe2a99b7eb22ccbf1fe0f9c122309
Benjamin Yu <benjaminlyu@gmail.com>
2012-11-04 08:27:05 -0800

    Clean up PolicyChecker Spec for complete code coverage.
    Details:
    * Used a mock instead of a proc that raises an error.
    * Remove unneeded parens for 'double'

--- spec/serf/middleware/policy_checker_spec.rb
+++ spec/serf/middleware/policy_checker_spec.rb
@@ -10,10 +10,9 @@ describe Serf::Middleware::PolicyChecker do
     context 'when policy raises error' do
 
       it 'should not call app' do
+        uncalled_mock = double 'uncalled mock'
         app = described_class.new(
-          proc { |parcel|
-            fail 'app was called'
-          },
+          uncalled_mock,
           policy_chain: [
             PassingPolicy.new,
             FailingPolicy.new,
@@ -29,7 +28,7 @@ describe Serf::Middleware::PolicyChecker do
     context 'when all policies pass' do
 
       it 'should call the app' do
-        parcel = double('parcel')
+        parcel = double 'parcel'
         parcel.should_receive :some_success
         app = described_class.new(
           proc { |parcel|
@@ -47,14 +46,14 @@ describe Serf::Middleware::PolicyChecker do
     it 'should iterate the policy chain' do
       count = 10
       policy_chain = (1..count).map { |i|
-        policy = double('policy')
+        policy = double 'policy'
         policy.should_receive(:check!).once do |parcel|
           parcel.check_called
         end
         policy
       }
 
-      parcel = double('parcel')
+      parcel = double 'parcel'
       parcel.should_receive(:check_called).exactly(count).times
       parcel.should_receive :some_success
 
