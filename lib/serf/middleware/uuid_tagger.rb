3c037cac6899af7c1bc22fb6bb99fd25e0d84d89
Benjamin Yu <benjaminlyu@gmail.com>
2012-11-03 19:26:27 -0700

    Serf::Middleware::UuidTagger only one header UUID.
    Details:
    * Before, the UuidTagger would add a parent_uuid and/or
      origin_uuid to the headers based on some semi-complicated logic
      if those fields were nil.
    * This was some how make a guess about the actual origin and parent
      of the incoming parcel. However, adding the guess actually could
      cause incorrect facts to be recorded.
    * Thus, we keep parent and origin fields nil if we just don't know.
    * But we actually tag the uuid field itself because we know that
      we have this particular request in.
    * It will be up to the log processing to handle nil values and
      make assumptions (or additional searches) about for parent and origin.

--- lib/serf/middleware/uuid_tagger.rb
+++ lib/serf/middleware/uuid_tagger.rb
@@ -25,27 +25,11 @@ module Middleware
     end
 
     def call(parcel)
-      # Get local reference to the request parcel's headers.
-      headers = parcel[:headers] || {}
+      # Makes sure our parcel has headers
+      parcel[:headers] ||= {}
 
       # Tag headers with a UUID unless it already has one
-      headers[:uuid] ||= uuidable.create_coded_uuid
-
-      # Ensures origin and parent UUIDs are set
-      if headers[:origin_uuid].nil? && headers[:parent_uuid].nil?
-        # Both origin and parent are blank.
-        headers[:origin_uuid] = headers[:uuid]
-        headers[:parent_uuid] = headers[:uuid]
-      elsif headers[:origin_uuid] && headers[:parent_uuid].nil?
-        # Origin is set, but parent is blank
-        headers[:parent_uuid] = headers[:origin_uuid]
-      elsif headers[:parent_uuid] && headers[:origin_uuid].nil?
-        # Parent is set, but origin is blank
-        headers[:origin_uuid] = headers[:parent_uuid]
-      end
-
-      # Reset the headers back into the parcel in case it was originally nil.
-      parcel[:headers] = headers
+      parcel[:headers][:uuid] ||= uuidable.create_coded_uuid
 
       # Pass on the given parcel with newly annotated headers
       app.call parcel
