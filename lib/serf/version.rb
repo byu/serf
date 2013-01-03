05d53530bc5149d82a300e3de79506234084345e
Benjamin Yu <benjaminlyu@gmail.com>
2013-01-03 00:11:48 -0800

    Version bump to v0.13.0

--- lib/serf/version.rb
+++ lib/serf/version.rb
@@ -2,8 +2,8 @@ module Serf
 
   module Version
     MAJOR = 0
-    MINOR = 12
-    PATCH = 1
+    MINOR = 13
+    PATCH = 0
     BUILD = nil
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join '.'
   end
